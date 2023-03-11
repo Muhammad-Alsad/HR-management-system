5c8b055cf23bc7c0cbb7f2a72f37cf83d4edafd8
muhammad <moalsad001@gmail.com>
2023-03-11 19:13:18 +0000

    add Dom m

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # HR-management-system
+
+#DOM-manipulation
+
+------------------------------------
+today project talk about Hr-MANAGMENT SYSTEM 
+
+
+
+
+----------------------------
+we learn through this project how to DOM manipulation and apply it so basically we apply like a chimp really have a different card and eat card have an ID at celery a full name of the bathroom for you. She's a junior or senior or Junior.
+
+
+--------------------------------------------------
+plus we learn how to reach an element through document dot get element by ID or tag name and plus a crate in her element as well
+
+---------------------------------------
+All what I talk about i apply it in a project .
+
+
--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const allEmployee =[];
+let newId=[]
+let Idgen = 1001;
+let mainDiv = document.getElementById("contentDiv");
+let form = document.getElementById("form");
 
 function newEmployee(EmpID,name,dep,level,image,Salary){
     this.EmployeeID=EmpID;
@@ -12,15 +16,18 @@ function newEmployee(EmpID,name,dep,level,image,Salary){
     allEmployee.push(this);
     
 }
+newEmployee.prototype.genId =function(){
+    this.EmployeeID=(newId +Idgen );
+    Idgen ++;
+}
 
-let GhaziSamer = new newEmployee(1000,"Ghazi Samer","Administration","Senior");
-let LanaAli = new newEmployee(1001,"Lana Ali","Finance","Senior");
-let TamaraAyoub = new newEmployee(1002,"Tamara Ayoub","Marketing","Senior");
-let SafiWalid = new newEmployee(1003,"Safi Walid","Administration","Mid-Senior");
-let OmarZaid = new newEmployee(1004,"Omar Zaid","Development","Senior");
-let RanaSaleh = new newEmployee(1005,"Rana Saleh","Development","Junior");
-let HadiAhmad = new newEmployee(1006,"Hadi Ahmad","Finance","Mid-Senior");
-
+let GhaziSamer = new newEmployee(1000,"Ghazi Samer","Administration","Senior","../assets/Ghazi.jpg");
+let LanaAli = new newEmployee(1001,"Lana Ali","Finance","Senior","../assets/Lana.jpg");
+//let TamaraAyoub = new newEmployee(1002,"Tamara Ayoub","Marketing","Senior","../assets/Tamara.jpg");
+//blet SafiWalid = new newEmployee(1003,"Safi Walid","Administration","Mid-Senior","../assets/Safi.jpg");
+// let OmarZaid = new newEmployee(1004,"Omar Zaid","Development","Senior");
+// let RanaSaleh = new newEmployee(1005,"Rana Saleh","Development","Junior");
+// let HadiAhmad = new newEmployee(1006,"Hadi Ahmad","Finance","Mid-Senior");
 
 newEmployee.prototype.calculating =function () {
     //debugger
@@ -40,14 +47,74 @@ newEmployee.prototype.netSalary =function () {
 }
 
 newEmployee.prototype.render =function () {
-    //debugger;
+
     this.netSalary();
-    document.write(`<h4>the name is ${this.FullName} && my net salary is ${this.Salary }</h4>`);
+    this.genId();
+    //document.write(`<h4>the name is ${this.FullName} && my net salary is ${this.Salary }</h4>`);
+    let divCad =document.getElementById("divCards");
+    //sec Div
+    let creDiv =document.createElement("div");
+    creDiv.className="card-style";
+    let imgEl =document.createElement("img");
+    imgEl.src= this.ImageURL;
+    creDiv.appendChild(imgEl);
+    divCad.appendChild(creDiv);
+    imgEl.style.width="100px";
+    mainDiv.appendChild(divCad);
+
+    let h4El = document.createElement("h4");
+    h4El.textContent =`Full Name : ${this.FullName}`;
+    creDiv.appendChild(h4El)
+    divCad.appendChild(creDiv);
+    mainDiv.appendChild(divCad);
+
+    let h4Id =document.createElement("h4");
+    h4Id.textContent=`Id : ${this.EmployeeID}`;
+    creDiv.appendChild(h4Id);
+    divCad.appendChild(creDiv);
+    mainDiv.appendChild(divCad);
+
+    let depEl =document.createElement("h4");
+    depEl.textContent=`Department : ${this.Department}`;
+    creDiv.appendChild(depEl)
+    divCad.appendChild(creDiv);
+    mainDiv.appendChild(divCad);
+
+    let levelEl = document.createElement("h4");
+    levelEl.textContent=`Level : ${this.level}`;
+    creDiv.appendChild(levelEl)
+    divCad.appendChild(creDiv);
+    mainDiv.appendChild(divCad);
+
+    let SalaryEl =document.createElement("h4");
+    SalaryEl.textContent=`Salary : ${this.Salary}`;
+    creDiv.appendChild(SalaryEl)
+    divCad.appendChild(creDiv);
+    mainDiv.appendChild(divCad);
+
+    this.genId();
+}
+
+form.addEventListener("submit",saveTheData);
+
+function saveTheData(event){
+    event.preventDefault();
+    alert("form successfully submitted !");
+    debugger;
+    let fullName = event.target.fname.value;
+    let ImgUrl = event.target.Img.value;
+    let Department = event.target.Department.value;
+    let level = event.target.Level.value;
+
+
+    let nEmployee=new newEmployee(fullName,ImgUrl,Department,level);
+    nEmployee.render();
 }
 
 function readEmp (){
     //debugger;
     for(let i=0;i<allEmployee.length;i++){
+        
         allEmployee[i].render();
     }
 }
